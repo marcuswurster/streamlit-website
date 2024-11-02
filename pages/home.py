9684386255cea8c23c71c4a12089307cf9b89a9a
marcuswurster <marcus.wurster@sevdesk.de>
2024-11-02 19:21:17 +0100

    Update home page

--- pages/home.py
+++ pages/home.py
@@ -1,6 +1,13 @@
 import streamlit as st
-from PIL import Image
-import os
 
 def run():
     st.title("Home")
+
+    st.write(
+        """
+        Hello, nice to meet you!
+        I am Marcus and a Data enthusiast from Rostock, Germany.
+        This is my little personal website built with Streamlit. 
+        Feel free to check out my CV, get in Contact with me or have a look at my personal Libarary of articles, books and blogs regarding data.
+        """
+    )
