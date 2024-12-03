7c19f5dc6be5d8b3cca7a4a1beccacd6b91c5bc1
marcuswurster <marcus.wurster@sevdesk.de>
2024-12-03 16:30:52 +0100

    Remove library page / Update home page

--- pages/home.py
+++ pages/home.py
@@ -8,6 +8,6 @@ def run():
         Hello, nice to meet you!
         I am Marcus and a Data enthusiast from Rostock, Germany.
         This is my little personal website built with Streamlit. 
-        Feel free to check out my CV, get in Contact with me or have a look at my personal Libarary of articles, books and blogs regarding data.
+        Feel free to check out my CV or get in contact with me.
         """
     )
